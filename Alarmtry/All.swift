12ed29434eed5ff2a50a2524f1b01e72a7dfd83e
Vincenzo Raucci <vincy.r.98@gmail.com>
2020-06-08 19:37:58 +0200

    initial commit

---
+++ Alarmtry/All.swift
@@ -0,0 +1,16 @@
+//
+//  All.swift
+//  Alarmtry
+//
+//  Created by Vincenzo Raucci on 31/01/2020.
+//  Copyright © 2020 _. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class EKAlarm : EKObject{
+    
+    init(relativeOffset offset: TimeInterval)
+
+}
